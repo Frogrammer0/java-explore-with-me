c92e1ef11e43b0a3f7205d408e6cdae4964001e0
Roman Golubev <roman222golubev@mail.ru>
2025-12-13 14:55:31 +0300

    update dto

--- main-service/src/main/java/ru/practicum/ewm/service/impl/UserServiceImpl.java
+++ main-service/src/main/java/ru/practicum/ewm/service/impl/UserServiceImpl.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.ewm.exception.ConflictException;
 import ru.practicum.ewm.exception.NotFoundException;
 import ru.practicum.ewm.service.UserService;
